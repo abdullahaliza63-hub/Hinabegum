8353171c2f14f05a3a481708a0c521ea437f2790
bgashyr648-ops <bgashyr648@gmail.com>
2026-09-23 19:29:14 +0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -150,7 +150,7 @@ ${menuSections}
                 forwardingScore: 999, 
                 isForwarded: true, 
                 forwardedNewsletterMessageInfo: { 
-                    newsletterJid: '120363411750982159@newsletter', 
+                    newsletterJid: '120363423987455239@newsletter', 
                     newsletterName: BOT_NAME, 
                     serverMessageId: 143 
                 } 
